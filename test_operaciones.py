dd022b886a3b53eb06c6102947651fc1c4710324
Leonor Analia Carrizo <leonor.analia.carrizo@ericsson.com>
2024-04-07 16:18:19 +0200

    Nueva función de resta y test unitario respectivo

--- test_operaciones.py
+++ test_operaciones.py
@@ -6,6 +6,11 @@ class TestOperaciones(unittest.TestCase):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+        
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
 
 if __name__ == '__main__':
     unittest.main()
