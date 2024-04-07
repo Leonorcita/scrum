5c51dfe43ba40badbf9fddea0ec483b7d229820c
Leonor Analia Carrizo <leonor.analia.carrizo@ericsson.com>
2024-04-07 16:50:58 +0200

    Modify function

--- test_operaciones.py
+++ test_operaciones.py
@@ -1,12 +1,12 @@
 import unittest
-from operaciones import sumar
+from operaciones import sumar,restar
 
 class TestOperaciones(unittest.TestCase):
     def test_sumar(self):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
-        
+
     def test_restar(self):
         self.assertEqual(restar(3, 2), 1)
         self.assertEqual(restar(-1, 1), -2)
