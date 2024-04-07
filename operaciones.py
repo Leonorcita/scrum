9be8e2596aadc69485eebc4f270c412653d51f91
Leonor Analia Carrizo <leonor.analia.carrizo@ericsson.com>
2024-04-07 17:22:23 +0200

    Agregamos funcion para multiplicar

--- operaciones.py
+++ operaciones.py
@@ -4,6 +4,10 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
+def multiplicar(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print(sumar(5, 3))
     print(restar(5, 3))
+    print(multiplicar(5, 3))
